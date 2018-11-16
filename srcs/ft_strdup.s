8cfbd40cfd7964e743532cf2b7764cce2b09f0b6
Nicolas BOUCHINET <nbouchin@e3r2p21.42.fr>
2018-11-16 16:31:04 +0100

    Update: strlen now works

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -5,15 +5,20 @@ extern _ft_strlen
 extern _malloc
 
 _ft_strdup:
-	enter 0, 0
-	push rdi					; push given parameter on stack 
+	push rbp					; push rbp register on stack
+	mov rbp, rsp				; move rsp register on rbp register
+	sub rsp, 16					; align memory
+	
+	mov [rbp - 8], rdi			; push rdi address on rbp - 8
 	call _ft_strlen				; get string length
-	push rax					; push string length on stack
+	mov [rbp - 16], rax			; push rax address on rbp - 16
 	mov rdi, rax				; give string length to malloc
 	call _malloc				; call malloc
 	mov rdi, rax				; put the result of malloc into first parameter
-	pop rdx						; pop back string length into rdx register
-	pop rsi						; pop back default string into rsi register
+	mov rsi, [rbp - 8]			; pop back default string into rsi register
+	mov rdx, [rbp - 16]			; pop back string length into rdx register	
 	call _ft_memcpy				; call _ft_memcpy
-	leave
+
+	mov rsp, rbp				; move rbp on rsp register
+	pop rbp						; pop rbp register from stack
 	ret
--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -2,10 +2,12 @@ section .text
 global _ft_strlen
 
 _ft_strlen:
-		mov rsi, rdi
-		mov rax, 0
-		repne scasb 
-		dec rdi
-		sub rdi, rsi
-		mov rax, rdi
-		ret
+	mov rsi, rdi
+	mov rax, 0
+	cld
+	;;not rcx
+	repne scasb 
+	dec rdi
+	sub rdi, rsi
+	mov rax, rdi
+	ret
