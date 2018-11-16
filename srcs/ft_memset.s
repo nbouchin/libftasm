bad9ea43fac63ecb38c0bf8a0b54bd0fc1cff86a
Nicolas BOUCHINET <nbouchin@e3r2p21.42.fr>
2018-11-16 11:36:27 +0100

    Update: add ft_memset

--- srcs/ft_memset.s
+++ srcs/ft_memset.s
@@ -1,11 +1,12 @@
 section .text
 global _ft_memset
+extern _ft_strlen
 
 _ft_memset:
 		mov rcx, rdx
-		mov rbx, rdi
-
-		mov [rbx + rcx - 1], byte rsi
-
+		mov rax, rsi
+		rep stosb 
+		sub rdi, rax
 		mov rax, rdi
+		inc rax
 		ret
